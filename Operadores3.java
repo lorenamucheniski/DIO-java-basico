0f62045d1f13bb0cdd2d4d02bcf2cad546ba50e0
lorenamucheniski <lomucheniski25@gmail.com>
2022-12-23 14:41:28 -0300

    incrementando um número

---
+++ Operadores3.java
@@ -0,0 +1,11 @@
+public class Operadores3 {
+	
+	public static void main (String[] args) {
+		int numero = 0;
+		while (numero < 10) {	
+			numero ++;
+		System.out.println(numero);
+		
+		}	
+	}	
+}
