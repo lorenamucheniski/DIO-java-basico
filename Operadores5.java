098afd9e33d261d6b00fd6aae28e41b51a3b8497
lorenamucheniski <lomucheniski25@gmail.com>
2022-12-25 20:34:49 -0300

    Operadores relacionais

---
+++ Operadores5.java
@@ -0,0 +1,21 @@
+public class Operadores5 {
+	
+	public static void main(String[] args) {
+		
+		int a,b;
+		a = 5;
+		b = 9;
+		
+		boolean resultado = a == b;
+		System.out.println("A é igual a B?   " + resultado);
+		
+		resultado = a != b;
+		System.out.println("A é diferente de B?  " + resultado);
+		
+		resultado = a > b;
+		System.out.println("A é maior que B?  " + resultado);
+		
+		resultado = a < b;
+		System.out.println("A é menor que B?  " + resultado);
+	}
+}
