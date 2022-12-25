fb3ab449956a6d1e5f9a1659f0786d899dd2a661
lorenamucheniski <lomucheniski25@gmail.com>
2022-12-25 19:58:09 -0300

    Operador ternário

---
+++ Operadores4.java
@@ -0,0 +1,16 @@
+public class Operadores4 {
+	
+	public static void main (String[] args) {
+		
+		int a,b;
+		a = 5;
+		b = 7;
+		
+		String resultado = a == b ? "Verdadeiro" : "Falso";
+		
+		System.out.println("A variavel A é igual a variavel B?");
+		System.out.println(resultado);
+		
+	}
+	
+}
