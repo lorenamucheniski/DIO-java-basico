5014e9d22ea9f6592067b021211997fc6dda2e8b
lorenamucheniski <lomucheniski25@gmail.com>
2022-12-29 10:10:45 -0300

    Executando programa no terminal

--- AboutMe.java
+++ AboutMe.java
@@ -1,7 +1,7 @@
 public class AboutMe {
 	
 	public static void main(String[] args) {
-		
+		// os argumentos começam com índice 0
 		String nome = args [0];
 		String sobrenome = args [1];
 		int idade = Integer.valueOf(args[2]);
