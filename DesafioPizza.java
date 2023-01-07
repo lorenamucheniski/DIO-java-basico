e5e1dff3d57dd034e3d8cc792198e3381f5c8008
lorenamucheniski <lomucheniski25@gmail.com>
2023-01-06 21:34:46 -0300

    Desafio da pizza

---
+++ DesafioPizza.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class DesafioPizza {
+	
+	 public static void main(String[] args) {
+		    final int SLICE = 4;
+		    Scanner input = new Scanner(System.in);
+		    
+		    int i = 0;
+		    
+		    System.out.println("Quantos testes? ");
+		    int T = input.nextInt();
+		    while (i <= T) {
+		    	i++;
+		    	System.out.println("Quantas pessoas? ");
+		    	float pessoas = input.nextInt();
+		    	System.out.println("Quantos pedacos? ");
+		    	float pedaco = input.nextInt();
+		    	
+		    float A = (pessoas * pedaco)/ SLICE;
+		    int B = (int)Math.ceil(A);
+		    
+		    System.out.println((B));
+		    break;
+		    } 	
+		    
+		    
+		    
+		    
+ 
+	 }
+}
