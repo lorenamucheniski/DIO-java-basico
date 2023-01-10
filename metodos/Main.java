e14fbc22f004007f83d63d3375073cd89457617c
lorenamucheniski <lomucheniski25@gmail.com>
2023-01-10 09:30:50 -0300

    Classe Main

--- metodos/Main.java
+++ metodos/Main.java
@@ -3,8 +3,26 @@ package metodos;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		//Calculadora
+		System.out.println("Exercício calculadora: ");
+		Calculadora.somar(15, 20);
+		Calculadora.subtrair(20, 9.8);
+		Calculadora.multiplicar(10, 2);
+		Calculadora.dividir(30, 5);
+		
+		//Mensagem
+		System.out.println("Bem vindo! Que horas são?");
+		Mensagem.obterMensagem(10);
+		Mensagem.obterMensagem(15);
+		Mensagem.obterMensagem(21);	
+		
+		//Emprestimo
+		System.out.println("Empréstimo Bancário");
+		System.out.println("Digite o valor que deseja e a quantidade de parcelas (2 ou 3):");
+		Emprestimo.calcular(1000, 2);
+		Emprestimo.calcular(850, 3);
 
 	}
-
+	
 }
