a74105f11163bd1904f995f22d12bced8ac1be29
lorenamucheniski <lomucheniski25@gmail.com>
2023-01-11 00:29:23 -0300

    Calculando area

--- calculo_area/Main.java
+++ calculo_area/Main.java
@@ -4,14 +4,26 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Área do quadrado: ");
+		// Area
+		System.out.println("Area do quadrado: ");
 		Area.calcular(5.5);
 		
-		System.out.println("Área do retângulo: ");
+		System.out.println("Area do retangulo: ");
 		Area.calcular(5.6, 3);
 		
-		System.out.println("Área do trapézio: ");
+		System.out.println("Area do trapezio: ");
 		Area.calcular(16, 10.5, 6.2);
+		
+		//Area2
+		double areaQuadrado = Area2.Calcular(7);
+		System.out.println("Area do quadrado: " + areaQuadrado);
+		
+		double areaRetangulo = Area2.calcular(10, 6.8);
+		System.out.println("Area do retangulo: " + areaRetangulo);
+		
+		double areaTrapezio = Area2.calcular(15, 9.9, 10);
+		System.out.println("Area do trapezio: " + areaTrapezio);
+		
 	
 
 	}
